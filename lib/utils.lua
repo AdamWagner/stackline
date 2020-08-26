3a34061eb4a2252bf02895ee85ad9eb179341066
adamwagner <awagnerjcu08@gmail.com>
2020-08-25 23:20:53 -0700

    Add utils.look() to see tables with metatable details

--- lib/utils.lua
+++ lib/utils.lua
@@ -258,12 +258,19 @@ function utils.pick(input, key_values) -- {{{
 end -- }}}
 
 function utils.p(data, howDeep) -- {{{
+    -- local logger = hs.logger.new('inspect', 'debug')
     local depth = howDeep or 3
     if type(data) == 'table' then
         print(hs.inspect(data, {depth = depth}))
+        -- logger.df(hs.inspect(data, {depth = depth}))
     else
         print(hs.inspect(data, {depth = depth}))
+        -- logger.df(hs.inspect(data, {depth = depth}))
     end
+end -- }}}
+
+function utils.look(obj) -- {{{
+    print(hs.inspect(obj, {depth = 2, metatables = true}))
 end -- }}}
 
 function utils.pdivider(str) -- {{{
