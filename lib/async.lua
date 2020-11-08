62234548712019d5b28861a73d0fed10ac42477a
adamwagner <awagnerjcu08@gmail.com>
2020-11-07 18:46:44 -0800

    Cleanup async.lua

--- lib/async.lua
+++ lib/async.lua
@@ -1,19 +1,15 @@
+-- adapted from: https://github.com/ImagicTheCat/Luaseq/blob/master/src/Luaseq.lua
 
---[[
-adapted from: https://github.com/ImagicTheCat/Luaseq/blob/master/src/Luaseq.lua
-
-┌───────┐
-│ USAGE │
-└───────┘
--- function makeCall()
---     local r = async()
---     hs.task.new(c.paths.getStackIdxs, function(a,b,c) 
---        r(a,b,c)    --  <- "r" can be called directly to resolve
---                    -- except not when passed as a callback to hammerspoon fns:
---                    -- https://github.com/Hammerspoon/hammerspoon/issues/932
---     end):start()
---     return r:wait()
--- end
+--[[ USAGE {{{
+function makeCall()
+    local r = async()
+    hs.task.new(c.paths.getStackIdxs, function(a,b,c) 
+       r(a,b,c)    --  <- "r" can be called directly to resolve
+                   -- except not when passed as a callback to hammerspoon fns:
+                   -- https://github.com/Hammerspoon/hammerspoon/issues/932
+    end):start()
+    return r:wait()
+end
 
 function makeAnother()
     r = async()
@@ -29,14 +25,13 @@ async(function()
     local a,b,c = makeAnother()
     print(a,b,c)
 end)
-]]
+-- }}} ]]
 
 local async = {}
 
--- TASK 
--- wait for task to return
--- return task return values
 local function task_wait(self)
+  -- wait for task to return
+  -- return task return values
   if self.val then return table.unpack(self.val) end -- already done, return values
 
   local co = coroutine.running()
@@ -45,10 +40,8 @@ local function task_wait(self)
   return coroutine.yield(co) -- wait for the task to return
 end
 
--- complete task ---------------------------------------------------------------
--- (multiple calls will do nothing)
--- ...: return values
 local function task_return(self, ...)
+  -- complete task (multiple calls will do nothing)
   if not self.val then
     self.val = {...}
     for _, co in ipairs(self) do
@@ -62,43 +55,19 @@ local meta_task = {
   __index = {
     wait = task_wait,
     resolve = task_return,
-
-    -- Adapted from: https://github.com/iagrib/AsyncLua
-    -- DOES NOT WORK as of 2020-10-17
-    -- get = function(self, cb)
-    --   assert(type(cb) == "function", "Awaitable:get(fn) - argument must be a function")
-    --   if self.val then
-    --     cb(unpack(self.val))
-    --   end
-    -- end
-
   },
   __call = task_return,
 }
 
--- async ----------------------------------------------------------------------
--- no parameters: create a task
---- return task
--- parameters: execute function as coroutine (shortcut)
---- f: function
 function async.async(f)
+  -- no parameters: create a task → return task
+  -- parameter: function
   if f then
     local co = coroutine.create(f)
     local ok, err = coroutine.resume(co)
-    -- This ↓ breaks luassert!
-    err = type(err)=='table' and table.concat(err, '\n') or err
-    if not ok then io.stderr:write(debug.traceback(co, "async: "..err).."\n") end
-    -- if not ok then return err end
+    if not ok then return err end
   else
     local new = {}
-    -- wrap in fn & pass new (self) as 1st param,
-    -- so that r.resolve can be passed as a callback to third-party async fns
-    -- E.g.,
-    --    function makeAnother()
-    --        r = async()
-    --        hs.task.new(c.paths.getStackIdxs, r.resolve):start()
-    --        return r:wait()
-    --    end
     new.resolve = function(...) task_return(new, ...) end
     return setmetatable(new, meta_task)
   end
