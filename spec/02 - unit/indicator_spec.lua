df75f34ff0753cd4a42f3d6ea7ae130ab7db430f
adamwagner <awagnerjcu08@gmail.com>
2020-11-22 08:04:34 -0800

    unit test: add indicator schema validator and set up initial unit tests for stackline's indicator class

--- spec/02 - unit/indicator_spec.lua
+++ spec/02 - unit/indicator_spec.lua
@@ -1,32 +1,47 @@
+local u = require 'lib.utils'
+local test_utils = helpers.test_utils
+local is_stackline_indicator = helpers.schemas.entities.is_stackline_indicator
+
 describe('#module #indicator', function()
 
-  -- setup(function()
-  --   local  = mock(t, true)
-  -- end)
-
-  before_each(function()
-    require 'lib.updatePackagePath'
-    u = require 'lib.utils'
-    _G.hs = helpers.reloadMock()
-
+  setup(function()
+    hs = helpers.reloadMock()
     local fixturePath = 'screen_state.three_S__seven_W_45315fe12c8ef6a1a7fbf3a3a2b013f6'
-    fixture = require 'spec.fixtures.load'()
-    hs.window.filter:set(fixture.screen.windows)
-    hs.task:set(fixture.stackIndexes)
-    stackline = mock(require 'stackline.stackline')
-    -- stackline.window = require 'stackline.window'
-    stackline:init()
+    fixture = test_utils.applyFixture(fixturePath)
+    test_utils.startStackline(fixture)
   end)
 
-  it('new()', function()
+  it('exists for each stacked win', function()
+    local indicators = {}
+    stackline.manager:eachWin(function(w)
+        table.insert(indicators, w.indicator)
+    end)
+    u.p(indicators[1])
 
-    -- stackline.manager:eachStack(function(s)
-    --   s:resetAllIndicators()
-    --   s:eachWin(function(w)
-    --     table.insert(indicators_reset, w.indicator.canvas)
-    --   end)
-    -- endn)
+    local indicator_keys = {"side", "rectIdx", "iconIdx", "canvas_rect", "canvas", "radius", "screen", "c", "showIcons", "frame", "width", "iconRadius", "stackFocus", "screenFrame", "fadeDuration", "icon_rect" }
 
+    -- u.p(u.keys(u.omit(indicators[1], 'win', 'config')))
+    u.p(u.omit(indicators[1], 'win', 'config'))
+
+
+    assert.equals(fixture.meta.num_total_wins, #indicators)
+
+
+
+  end)
+
+  it('match schema', function()
+    stackline.manager:eachWin(function(w)
+      local indicator = u.omit(w.indicator, 'config', 'win')
+      local ok, err = is_stackline_indicator(indicator)
+      if not ok then assert.is_true(err) end
+    end)
+  end)
+
+
+
+
+  it('Experiment with spies & mocks', function()
     stackline.manager = mock(stackline.manager)
     stackline.window = mock(stackline.window)
 
@@ -34,14 +49,15 @@ describe('#module #indicator', function()
     stackline.manager:update()
 
     -- u.p(stackline.manager)
-    assert.spy(stackline.manager.update).was_called(2)
+    -- assert.spy(stackline.manager.update).was_called(2)
 
 
-    -- hs.logger.new('fuck')
-    -- hs.logger.new('fucking')
-    -- hs.logger.new('fucked')
-    -- assert.spy(hs.logger.new).was_called_with('fuck')
+    -- hs.logger.new('test')
+    -- hs.logger.new('testing')
+    -- hs.logger.new('tested')
+    -- assert.spy(hs.logger.new).was_called_with('test')
     -- assert.spy(hs.logger.new).was_called(3)
   end)
+
 
 end)
--- spec/helpers/schemas/entity_schemas.lua
+++ spec/helpers/schemas/entity_schemas.lua
@@ -1,23 +1,47 @@
 local v = require 'stackline.lib.valid'
 local o = v.optional
 
-local unwrap = function(x)
+local unwrap = function(x)  -- {{{
   return  v.is_table {
     value = x
   }
-end
+end  -- }}}
 
-local is_frame  = function()
-    return v.is_table {
+local is_color = function()  -- {{{
+  return v.is_table {
+    white = o(v.is_number()),
+    red   = o(v.is_number()),
+    green = o(v.is_number()),
+    blue  = o(v.is_number()),
+    alpha = o(v.is_number()),
+  }
+end  -- }}}
+
+local is_frame  = function()  -- {{{
+  return v.or_op(
+    v.is_table {
       _h = v.is_number(),
       _w = v.is_number(),
       _x = v.is_number(),
       _y = v.is_number(),
+    },
+    v.is_table {
+      h = v.is_number(),
+      w = v.is_number(),
+      x = v.is_number(),
+      y = v.is_number(),
+    }
+  )
+end  -- }}}
+
+local is_coordinates  = function()  -- {{{
+  return v.is_table {
+    x = v.is_number(),
+    y = v.is_number(),
   }
-end
+end  -- }}}
 
 local M = {}
-
 
 -- Hammerspoon schemas
 -- ———————————————————————————————————————————————————————————————————————————
@@ -45,19 +69,60 @@ M.is_hs_win = v.is_table {
 -- ———————————————————————————————————————————————————————————————————————————
 
 -- TODO: stackline Indicator schema
+M.is_stackline_indicator = v.is_table {
+  showIcons  = v.is_boolean(),
+  stackFocus = v.is_boolean(),
+  side       = v.in_list{'right', 'left'},
+  width      = v.is_number(),
 
+  c = v.is_table {
+    alpha        = v.is_number(),
+    color        = is_color(),
+    dimmer       = v.is_number(),
+    fadeDuration = v.is_number(),
+    iconDimmer   = v.is_number(),
+    iconPadding  = v.is_number(),
+    offset       = is_coordinates(),
+    pillThinness = v.is_number(),
+    radius       = v.is_number(),
+    shouldFade   = v.is_boolean(),
+    showIcons    = v.is_boolean(),
+    size         = v.is_number(),
+    vertSpacing  = v.is_number(),
+  },
 
+  canvas      = is_frame(),
+  canvas_rect = is_frame(),
+  frame       = is_frame(),
+  screenFrame = is_frame(),
+  icon_rect   = is_frame(),
+
+  fadeDuration = v.is_number(),
+  iconIdx      = v.is_number(),
+  iconRadius   = v.is_number(),
+  radius       = v.is_number(),
+  rectIdx      = v.is_number(),
+
+  screen = v.is_table {
+    id        = unwrap(v.is_number()),
+    frame     = unwrap(is_frame()),
+    fullFrame = unwrap(is_frame()),
+  }
+}
+
+-- stackline window
 M.is_stackline_win = v.is_table {
-    app        = v.is_string(),
-    -- app        = v.is_boolean(), -- just for testing
-    frame      = is_frame(),
-    id         = v.is_number(),
-    screen     = v.is_number(),
-    stackId    = v.is_string(),
-    stackIdFzy = v.is_string(),
-    title      = v.is_string(),
-    topLeft    = v.is_string(),
-    _win       = M.is_hs_win,
+  app        = v.is_string(),
+  -- app        = v.is_boolean(), -- use to confirm that failing validations fail tests
+  frame      = is_frame(),
+  id         = v.is_number(),
+  screen     = v.is_number(),
+  stackId    = v.is_string(),
+  stackIdFzy = v.is_string(),
+  title      = v.is_string(),
+  topLeft    = v.is_string(),
+  _win       = M.is_hs_win,
+  indicator  = o(M.is_stackline_indicator),
 }
 
 M.are_stackline_wins = v.is_array(M.is_stackline_win)
